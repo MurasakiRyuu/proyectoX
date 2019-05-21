3e4de1acb47f600dd73831717616643756ba08ef
MurasakiRyuu <joseangel_94@hotmail.com>
2019-05-21 16:02:18 -0500

    saludo.hbs

--- Desktop/pruebaheroku/proyectoX/routes/index.js
+++ Desktop/pruebaheroku/proyectoX/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hola', function(req, res, next){
+	//solicitud a una ruta API REST
+	res.render('saludo', { OtraCosa: 'cosa'});
+});
+
 module.exports = router;
